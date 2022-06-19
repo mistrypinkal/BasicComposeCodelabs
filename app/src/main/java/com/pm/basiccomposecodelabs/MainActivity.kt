5a4a8186d9579b3414629d70aa9bfd96d7c3e33e
Pinkal Mistry <pinkald.mistry@gmail.com>
2022-06-19 12:23:21 +0400

    Step 1: Search bar - Modifiers added

--- app/src/main/java/com/pm/basiccomposecodelabs/MainActivity.kt
+++ app/src/main/java/com/pm/basiccomposecodelabs/MainActivity.kt
@@ -3,29 +3,63 @@ package com.pm.basiccomposecodelabs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.pm.basiccomposecodelabs.ui.theme.BasicComposeCodelabsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Android")
+            MySootheApp()
         }
     }
 }
 
+// Step 1: Search bar - Modifiers
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SearchBar(
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface
+        ),
+        placeholder = {
+            Text(stringResource(R.string.placeholder_search))
+        },
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
+    )
+}
+
+// Step: MySoothe App - Scaffold
+@Composable
+fun MySootheApp() {
+    // Implement composable here
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BasicComposeCodelabsTheme {
-        Greeting("Android")
+        MySootheApp()
     }
 }
